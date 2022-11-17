076b8ae8bbf4ec9d877542ae890f9e591fe46ca5
Keonil <69555662+hehe1004@users.noreply.github.com>
2022-11-17 19:36:16 +0900

    54

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -2,9 +2,12 @@ package com.cos.photogramstart.service;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -12,10 +15,19 @@ public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
 
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> 구독리스트(int pricipalId, int pageUesrId) {
+
+        return null;
+
+    }
+
     @Transactional
-    public void 구독하기(int fromUserId, int toUserId){
+    public void 구독하기(int fromUserId, int toUserId) {
+
         try {
-            subscribeRepository.mSubscribe(fromUserId,toUserId);
+            subscribeRepository.mSubscribe(fromUserId, toUserId);
 
         } catch (Exception e) {
             throw new CustomApiException("이미 구독을 하였습니다.");
