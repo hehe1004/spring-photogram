7e1c2ad0b1244cfd327046015b9042d6b0f373b9
Keonil <69555662+hehe1004@users.noreply.github.com>
2022-12-05 17:13:32 +0900

    6강 likes api

--- src/main/java/com/cos/photogramstart/web/api/ImageApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/ImageApiController.java
@@ -4,6 +4,7 @@ package com.cos.photogramstart.web.api;
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.service.ImageService;
+import com.cos.photogramstart.service.LikesService;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageApiController {
 
     private final ImageService imageService;
+    private final LikesService likesService;
 
     @GetMapping("/api/image")
     public ResponseEntity<?> imageStory(@AuthenticationPrincipal PrincipalDetails principalDetails,
@@ -30,5 +34,21 @@ public class ImageApiController {
 
 
     }
+
+    @PostMapping("/api/image/{imageId}/likes")
+    public ResponseEntity<?> likes(@PathVariable int imageId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        likesService.좋아요(imageId, principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1,"좋아요 성공", null),HttpStatus.CREATED);
+
+    }
+
+
+    @PostMapping("/api/image/{imageId}/likes")
+    public ResponseEntity<?> unLikes(@PathVariable int imageId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        likesService.좋아요취소(imageId, principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1,"좋아요취소 성공", null),HttpStatus.OK);
+
+    }
+
 
 }
